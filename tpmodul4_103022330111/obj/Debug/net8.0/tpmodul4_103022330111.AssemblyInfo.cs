3ad9b56d5571d60d34e99bf16c5c5bd7bc8e0059
Pieter Immanuel Sinaga <pieterimmanuel04@gmail.com>
2025-03-10 23:10:10 +0700

    menambahkan implementasi dengan table-driven

--- tpmodul4_103022330111/obj/Debug/net8.0/tpmodul4_103022330111.AssemblyInfo.cs
+++ tpmodul4_103022330111/obj/Debug/net8.0/tpmodul4_103022330111.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul4_103022330111")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6d548253660634fb96ae7edd3014301cc180325")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul4_103022330111")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul4_103022330111")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
